0191e4ddba0e796c83d834a867fe790e341727d1
Felipe Neves Ricardo <felipe.neves@taglivros.com.br>
2022-05-23 13:55:46 -0300

    Next gen JS features

--- src/objs-arrays-enums.ts
+++ src/objs-arrays-enums.ts
@@ -20,12 +20,12 @@
 
 enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' };
 
-const person = {
-  name: 'Maximilian',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN
-};
+// const person = {
+//   name: 'Maximilian',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: Role.ADMIN
+// };
 
 // person.role.push('admin');
 // person.role[1] = 10;
@@ -35,13 +35,13 @@ const person = {
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
 
-console.log(person.name);
+// console.log(person.name);
 
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-  // console.log(hobby.map()); // !!! ERROR !!!
-}
+// for (const hobby of person.hobbies) {
+//   console.log(hobby.toUpperCase());
+//   // console.log(hobby.map()); // !!! ERROR !!!
+// }
 
-if (person.role === Role.AUTHOR) {
-  console.log('is author');
-}
+// if (person.role === Role.AUTHOR) {
+//   console.log('is author');
+// }
--- src/unknown-never.ts
+++ src/unknown-never.ts
@@ -1,11 +1,11 @@
-let userInput: unknown;
-let userName: string;
+// let userInput: unknown;
+// let userName: string;
 
-userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string') {
-  userName = userInput;
-}
+// userInput = 5;
+// userInput = 'Max';
+// if (typeof userInput === 'string') {
+//   userName = userInput;
+// }
 
 function generateError(message: string, code: number): never {
   throw { message: message, errorCode: code };
